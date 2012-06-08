aaafd094da4563173dde0beba3d8495e0f325962
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2012-06-08 18:32:33 +0100

    Referece codes now have letters and digits.

--- src/org/instantplaces/im/server/dao/ReferenceCodeGeneratorDAO.java
+++ src/org/instantplaces/im/server/dao/ReferenceCodeGeneratorDAO.java
@@ -14,11 +14,15 @@ import com.googlecode.objectify.annotation.Unindexed;
 public class ReferenceCodeGeneratorDAO {
 
 	@NotSaved
-	private static final int MAX_CODE = 500;
+	private static final int CHARACTER_COUNT = 3;
 
 	@Unindexed
-	private ArrayList<Integer> codes;
+	private ArrayList<String> codes;
 
+	@NotSaved
+	private String symbols = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+	
 	@NotSaved
 	private PlaceDao place;
 
@@ -31,7 +35,7 @@ public class ReferenceCodeGeneratorDAO {
 	private Long referenceCodeGeneratorId;
 
 	public ReferenceCodeGeneratorDAO(PlaceDao place) {
-		codes = new ArrayList<Integer>(1000);
+		codes = new ArrayList<String>(100);
 		this.rebuild();
 		// Log.get().debug("ReferenceCodeManager: " + this.codes.toString());
 		this.setPlace(place);
@@ -41,23 +45,23 @@ public class ReferenceCodeGeneratorDAO {
 	private ReferenceCodeGeneratorDAO() {
 	}
 
-	public ArrayList<Integer> getCodes() {
+	public ArrayList<String> getCodes() {
 		return this.codes;
 	}
 
-	public int getNextCode() {
+	public String getNextCode() {
 		if (null == codes || codes.size() < 1) {
 			Log.get().error("No more codes to serve!");
-			return 0;
+			return null;
 		}
 
-		Integer i = codes.remove(0);
-		return i.intValue();
+		String i = codes.remove(0);
+		return i;
 	}
 
 	public synchronized String getNextCodeAsString() {
 		// TODO: Check nulls!!!!
-		return String.format("%03d", this.getNextCode());
+		return  this.getNextCode();
 	}
 
 	/**
@@ -69,9 +73,67 @@ public class ReferenceCodeGeneratorDAO {
 
 	public void rebuild() {
 		this.codes.clear();
+		
+
+		int maxIndex = this.symbols.length()-1;
+		int []indexes = new int[CHARACTER_COUNT];
+		for (int i = 0; i < indexes.length; i++ ) {
+			indexes[i] = 0;
+		}
+		//indexes[0] = 10; // we enforce an alphabetic character on the code, so we might as well start with one.
+		
+		boolean end = false;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while ( !end ) {
+			sb.setLength(0);
+			
+			int digitCount = 0;
+			int letterCount = 0;
+			for ( int i = 0; i < CHARACTER_COUNT; i++ ) {
+				/*
+				 * Count the number of digits and the number of letters
+				 */
+				if ( indexes[i] < 10 ) {
+					digitCount++;
+				} 
+				if ( indexes[i] >= 10 ) {
+					letterCount++;
+				}
+				sb.append( symbols.charAt(indexes[i]) );
+			}
+			
+			/*
+			 * We want to make sure that every reference code has at least one digit and one letter
+			 */
+			if (digitCount < CHARACTER_COUNT && letterCount < CHARACTER_COUNT) {
+				codes.add(sb.toString());
+			}
+			
+			
+			for ( int i = 0; i < CHARACTER_COUNT; i++ ) {
+				indexes[i]++;
+				
+				if ( indexes[i] > maxIndex ) {
+					indexes[i] = 0;
+				} else {
+					break;
+				}
+			}
+			
+			end = true;
+			for ( int i = 0; i < CHARACTER_COUNT; i++ ) {
+				if ( indexes[i] < maxIndex ) {
+					end = false;
+				}
+			}		
+			
+		}
+		/*
 		for (int i = 0; i < MAX_CODE; i++) {
 			codes.add(new Integer(i));
-		}
+		}*/
 	}
 
 	/*
@@ -121,32 +183,22 @@ public class ReferenceCodeGeneratorDAO {
 	 */
 
 	public void recycleCode(String code) {
-		try {
-			Integer c = Integer.valueOf(code);
-			if (!this.codes.contains(c)) {
-				this.codes.add(c);
+
+			
+			if ( !this.codes.contains(code) ) {
+				this.codes.add(code);
 			}
 
-		} catch (NumberFormatException e) {
-			Log.get().error(e.getMessage());
-			e.printStackTrace();
-
-		}
 	}
 
 	public void remove(String code) {
-		try {
-			Integer c = Integer.valueOf(code);
-			this.codes.remove(c);
 
-		} catch (NumberFormatException e) {
-			Log.get().error(e.getMessage());
-			e.printStackTrace();
+			this.codes.remove(code);
 
-		}
+		
 	}
 
-	public void setCodes(ArrayList<Integer> codes) {
+	public void setCodes(ArrayList<String> codes) {
 		this.codes = codes;
 
 	}
